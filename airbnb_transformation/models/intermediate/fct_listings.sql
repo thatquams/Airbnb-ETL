1b9367c3884869a0324f26da6e8c812dc85e4686
thatquams <devquams@gmail.com>
2025-05-20 08:21:49 +0100

    updated listing date_key

--- airbnb_transformation/models/intermediate/fct_listings.sql
+++ airbnb_transformation/models/intermediate/fct_listings.sql
@@ -12,15 +12,17 @@ WITH fct_listings AS (
 
     SELECT {{ dbt_utils.generate_surrogate_key(['l.id']) }} AS listing_key,
             {{ dbt_utils.generate_surrogate_key(['l.host_id']) }} AS host_key,
-            {{dbt_utils.generate_surrogate_key(['l.created_at']) }} AS date_key,
+            {{ date_formatting ('l.created_at') }} AS listing_created_at,
             l.name AS listing_name, l.room_type, l.minimum_nights, 
             {{ normalize_price('l.price') }} AS listing_price,
-            {{date_formatting("l.updated_at") }} AS updated_at
 
     FROM {{ ref("stg_airbnb_listings") }} l
+), 
+final AS (
+    SELECT listing_key, host_key,{{ dbt_utils.generate_surrogate_key(['listing_created_at']) }} AS date_key,
+    listing_name, room_type, minimum_nights, listing_price, listing_created_at FROM fct_listings
+
 )
 
-
-
 SELECT * 
-FROM fct_listings
+FROM final
